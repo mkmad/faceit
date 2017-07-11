69e442adefaeef6ab2447cc15ab7576178e4e26e
Mohan Madhavan <mohan.madhavan@rackspace.com>
2017-07-10 22:31:03 -0500

    Initial Commit

---
+++ faceit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  faceit
+//
+//  Created by Mohan Madhavan on 7/10/17.
+//  Copyright © 2017 Mohan Madhavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
